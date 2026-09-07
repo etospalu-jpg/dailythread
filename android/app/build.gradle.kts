c6852b2e115020198076dacffcfeeafc3083819c
etospalu-jpg <etospalu@gmail.com>
2026-09-07 16:41:11 +0800

    release(android): bump Daily Thread to 0.4.0-rc2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dailythread.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0-rc1"
+        versionCode = 5
+        versionName = "0.4.0-rc2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "SUPABASE_URL", "\"https://kmrgpityqzksuqvuqyfv.supabase.co\"")
